774f472931c59b97d34487f118b3458c32235636
LiuHaotong <liuhaotong0517@163.com>
2022-09-09 22:23:16 +0800

    9.9_2

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 # Python Basic Syntax
+# 9.
 import keyword
 print(keyword.kwlist)
 print(type(keyword))
 print('H' in 'HKU')
 print('h' in 'HKU')
 print('HKU'[::-1])
+
+print(1 == True)
+a = [1, 2, 3]
+c = [1, 2, 3]
+print(a == c)
+print(a is c)
+print(ord('😂'))
+print(ord('桐'))
+print(chr(128515))
